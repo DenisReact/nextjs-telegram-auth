8161c5b85b95c8873caba9b7f0c33f100fd4d28f
DenisReact <den.lopatinski.dev@gmail.com>
2024-11-22 17:28:25 +0200

    add loading status to protected page

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -2,42 +2,54 @@
 
 import React from "react";
 import { useRouter } from "next/navigation";
-import { Button } from "@app/@/components/ui/button";
 import { ITelegramUser } from "@app/types/ITelegramUser";
+import Image from "next/image";
+import LogoImage from "@app/public/globe.svg"; // change to your logo
 
 export default function ProtectedPage() {
   const [user, setUser] = React.useState<ITelegramUser | null>(null);
+  const [loading, setLoading] = React.useState(true);
   const router = useRouter();
 
   React.useEffect(() => {
     const userStr = localStorage.getItem("telegramUser") || "";
-    const userData: ITelegramUser = JSON.parse(userStr);
+    let userData: ITelegramUser | null = null;
 
-    if (!userData) {
-      router.push("/login");
+    if (userStr) {
+      try {
+        userData = JSON.parse(userStr);
+      } catch (error) {
+        console.error("Failed to parse user data:", error);
+      }
     }
 
-    setUser(userData);
+    // Simulate loading time
+    const timer = setTimeout(() => {
+      if (!userData) {
+        router.push("/login");
+      }
+
+      setUser(userData);
+      setLoading(false);
+    }, 3000); // 3 seconds
+
+    return () => clearTimeout(timer);
   }, [router]);
 
-  const onLogout = () => {
-    localStorage.removeItem("telegramUser");
-    // Remove the cookie by setting its expiration date to a past date
-    document.cookie = "https://oauth.telegram.org=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-    router.push("/login");
-  };
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center min-h-screen animate-pulse">
+        <Image src={LogoImage} alt="Loading..." width={60} height={60} className="shadow-sm rounded-full" />
+      </div>
+    );
+  }
 
-  if (!user) return null;
-
-  console.log(user);
+  if (!user) return <h2 className="text-xl">Failed to load user data</h2>;
 
   return (
     <div className="flex flex-col gap-4 items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-2xl font-bold">Welcome to the Protected Page!</h1>
-      <p>Check user data in console</p>
-      <Button className="uppercase" variant="default" onClick={onLogout}>
-        logout
-      </Button>
+      <p>Username: {user.username}</p>
     </div>
   );
 }
